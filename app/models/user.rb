1ad67bd148d08c2e812071dbf95d1d4fd560da62
chrischep <christine.chepleting@student.moringaschool.com>
2022-11-07 12:31:36 +0300

    code challenge complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.order(:star_rating).last.product
+   end 
+   def favorite_product
+        self.reviews.where(product_id: product.product.id).delete_all
+        
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,11 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+
+review1 = Review.create(star_rating: 6, comment: "This is a good product", user_id: user1.id, product_id: product1.id)
+review2 = Review.create(star_rating: 10, comment: "I like it very much", user_id: user2.id, product_id: product2.id)
+review3 = Review.create(star_rating: 7, comment: "This is a beautiful product", user_id: user1.id, product_id: product3.id)
+review4 = Review.create(star_rating: 2, comment: "ugly, unsatisfied", user_id: user3.id, product_id: product2.id)
+
 
 puts "Seeding done!"
